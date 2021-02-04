16ad18d67d851226016a01516306840aa024bf36
Max <mdunaevs@andrew.cmu.edu>
2021-02-03 20:30:18 -0500

    Finished chore model and passed tests

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -1,4 +1,23 @@
 class Chore < ApplicationRecord
     belongs_to :child
     belongs_to :task
+
+    validates_date :due_on
+
+    scope :alphabetical, -> {order(:name)}
+    scope :chronological, -> {order(:due_on)}
+    scope :pending, -> {where(completed: false)}
+    scope :done, -> {where(completed: true)}
+    scope :upcoming, -> {where("due_on >= ?", Date.today)}
+    scope :past, -> {where("due_on < ?", Date.today)}
+    scope :by_task, -> {joins(:task).order("tasks.name")}
+
+    def status
+        if self.completed 
+            "Completed"
+        else
+            "Pending"
+        end
+    end
+
 end

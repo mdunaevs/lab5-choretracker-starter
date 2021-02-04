50cf666daf94d3afc94c3ee015513d17edab3f9c
Max <mdunaevs@andrew.cmu.edu>
2021-02-03 20:14:17 -0500

    Finished task model and passed tests

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,13 @@
 class Task < ApplicationRecord
+    has_many :chores
+    has_many :children, through: :chores
+
+    validates_presence_of :name
+    validates :points, numericality: { only_integer: true, greater_than: 0 }
+
+    scope :alphabetical, -> {order(:name)}
+    scope :active, -> {where(active: true)}
+    
+
+
 end
